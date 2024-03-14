cd7da676d2b85826ee2ec5d82d51c074e622da2f
Mert Durkaya <mertdurkaya@gmail.com>
2024-03-14 15:32:57 +0300

    Initial Commit

---
+++ SwiftCrypto/SwiftCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoApp.swift
+//  SwiftCrypto
+//
+//  Created by Mert Durkaya on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
